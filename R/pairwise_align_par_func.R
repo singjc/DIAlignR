cd443a53ee6afd059dcb8c1eb805249ac014174d
Justin Sing <justincsing@gmail.com>
2020-08-02 03:18:15 -0400

    [DEBUG] Debugging parallel issues

--- R/pairwise_align_par_func.R
+++ R/pairwise_align_par_func.R
@@ -103,6 +103,7 @@ pairwise_align_par_func <- function( oswdata_runpair_data, XICs.ref, function_pa
     cat( sprintf("adaptiveRT: %s", adaptiveRT), file = function_param_input$redirect_output , sep = "\n" )
     cat( sprintf("XICs.ref:"), file = function_param_input$redirect_output , sep = "\n" )
     apply(XICs.ref, 1, function(x){cat(head(x), sep = '\t', file = function_param_input$redirect_output); cat("\n", file=function_param_input$redirect_output)})
+
     cat( sprintf("dim(XICs.ref): %s\ndim(XICs.eXp): %s\nadaptiveRT: %s\n", 
                  paste(unlist(lapply(XICs.ref, function(x) length(x[[1]]))), collapse=", "), paste(unlist(lapply(XICs.eXp, function(x) length(x[[1]]))), collapse=", "), file = function_param_input$redirect_output , sep = "\n"  )
 
